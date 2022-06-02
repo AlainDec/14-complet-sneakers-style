f1a7e57afdad9afb4fb8089c538a293f0b7d0d05
unknown <jtwest@free.fr>
2022-06-02 16:42:08 +0200

    add the search bar and its functionalities

--- App.tsx
+++ App.tsx
@@ -1,25 +1,45 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, ScrollView, View } from 'react-native';
+import { StyleSheet, ScrollView, View, Text } from 'react-native';
 import PRODUITS from './assets/data/data';
-import CardHeader from './components/CardHeader';
-import CardBody from './components/CardBody';
-import CardFooter from './components/CardFooter';
 import CardItem from './components/CardItem';
+import { Searchbar } from 'react-native-paper';
 
 export default function App() {
+
+  const [searchQuery, setSearchQuery] = useState('');
+  const onChangeSearch = (query: string) => setSearchQuery(query);
+
+  const [countTotalValues, setCountTotalValues] = useState(PRODUITS.length);
+
+  // Filtre la recherche depuis le champs du formulaire
+  const search = PRODUITS.filter( (value) => {
+    if (searchQuery === '' || 
+        value.title.toLowerCase().includes(searchQuery.toLowerCase())) {
+      
+      return value;
+    }
+  });
+
   return (
     <View style={styles.container}>
+      <Searchbar
+        placeholder="Search"
+        onChangeText={onChangeSearch}
+        value={searchQuery}
+      />
+
       <ScrollView>
+
+        <Text style={styles.countProducts}>{search.length} produits trouvés sur {countTotalValues}</Text>
+
         <View style={styles.cardContainer} >
           {
-            PRODUITS.map((value, index) => (
-
+            search.map((value, index) => (
               <View style={styles.card} key={index}>
                 <CardItem product={value} />
               </View>
-
-            )
-            )
+            ))
           }
         </View>
         <StatusBar style="auto" />
@@ -37,10 +57,15 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     marginHorizontal: 8,
+    marginTop: 20,
     justifyContent: 'space-between',
   },
   card: {
     width: '49%',
     marginBottom: 14,
+  },
+  countProducts: {
+    marginLeft: 8,
+    marginTop: 8,
   }
 });
